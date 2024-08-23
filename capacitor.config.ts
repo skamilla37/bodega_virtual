095b70a58200ce9d7923a67f1f75392dcf1e6843
skamilla37 <ce.escamilla@duocuc.cl>
2024-08-23 18:29:52 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bodega_virtual',
+  webDir: 'www'
+};
+
+export default config;
