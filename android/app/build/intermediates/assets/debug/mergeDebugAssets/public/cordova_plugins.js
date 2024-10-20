b6388134631eb819feb35657605507d149cb2dfb
skamilla37 <ce.escamilla@duocuc.cl>
2024-10-20 20:58:03 -0300

    .

---
+++ android/app/build/intermediates/assets/debug/mergeDebugAssets/public/cordova_plugins.js
@@ -0,0 +1,29 @@
+
+  cordova.define('cordova/plugin_list', function(require, exports, module) {
+    module.exports = [
+      {
+          "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+          "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+          "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+          "cordova.plugins.barcodeScanner"
+        ]
+        },
+      {
+          "id": "cordova-sqlite-storage.SQLitePlugin",
+          "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+          "pluginId": "cordova-sqlite-storage",
+        "clobbers": [
+          "SQLitePlugin"
+        ]
+        }
+    ];
+    module.exports.metadata =
+    // TOP OF METADATA
+    {
+      "cordova-sqlite-storage": "6.1.0",
+      "phonegap-plugin-barcodescanner": "8.1.0"
+    };
+    // BOTTOM OF METADATA
+    });
+    
